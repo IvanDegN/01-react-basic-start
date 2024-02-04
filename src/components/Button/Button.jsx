3ee5eeb81aa9fa204c6195a057c8ff46555b6d3d
IvanDegN <mauzer.bruno@yandex.ru>
2024-02-04 14:40:11 +0300

    add state in Feedback component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import classes from './Button.module.css'
 
 const Button = ({children, onClick, isActive}) =>{
 
-console.log(classes)
+//console.log(classes)
 
     return(
         <button className={ isActive ? `${classes.button} ${classes.active}` : classes.button  } onClick={onClick}>{children}</button>
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,7 +1,49 @@
+import { useState } from 'react'
+import Button from './Button/Button'
+
 const Feedback = () =>{
+
+    const [inputValue, setInputValue] = useState('')
+    const [selectValue, setSelectValue] = useState('')
+
+    const changeValueInput = (event) =>{
+        const name = event.target.value;
+       // console.log(name);
+        setInputValue(name)
+    }
+
+    const changeValueSelect = (event) =>{
+        const reason = event.target.value
+        // console.log(reason);
+        setSelectValue(reason)
+    }
+
     return(
         <section>
             <h3>Обратная связь</h3>
+
+            <form action="">
+                <label htmlFor="name">Ваше имя</label>
+                <input type="text" id="name" className="control" value={inputValue} onChange={changeValueInput}/>
+
+                <label htmlFor="reason">Причина обращения</label>
+                <select name="" id="reason" className="control" value={selectValue} onChange={changeValueSelect}>
+                    <option value="Ошибка">Ошибка</option>
+                    <option value="Нужна помощь">Нужна помощь</option>
+                    <option value="Предложение">Предложение</option>
+                </select>
+            </form>
+
+            <pre>
+                Name: {inputValue}
+                <br />
+                Select: {selectValue}
+            </pre>
+
+            <Button>Отправить</Button>
+
+            
+
         </section>
     )
 }
