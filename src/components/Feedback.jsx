a94a6eb1e9246dc5f64a8dc2b09d03bdc7f8b730
IvanDegN <mauzer.bruno@yandex.ru>
2024-02-05 00:12:52 +0300

    transforming state to object in Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -3,44 +3,71 @@ import Button from './Button/Button'
 
 const Feedback = () =>{
 
-    const [inputValue, setInputValue] = useState('')
-    const [selectValue, setSelectValue] = useState('')
-    const [error, setError] = useState(false)
+    const [form, setForm] = useState({
+        inputValue: '',
+        selectValue: 'Предложение',
+        error: false,
+    });
+
+    // const [inputValue, setInputValue] = useState('')
+    // const [selectValue, setSelectValue] = useState('')
+    // const [error, setError] = useState(false)
 
     const changeValueInput = (event) =>{
-        const name = event.target.value;
-        setInputValue(name)
-        setError(name.trim().length === 0)
+        // const name = event.target.value;
+        // setInputValue(name)
+        // setError(name.trim().length === 0)
+        setForm(prev => (
+            {...prev,
+            inputValue:  event.target.value,
+            error: event.target.value.trim().length === 0, 
+        }) )
+        // setForm({
+            
+        //     selectValue: form.selectValue
+        // })
     }
 
-    const changeValueSelect = (event) =>{
-        const reason = event.target.value
-        setSelectValue(reason)
-    }
+    // const changeValueSelect = (event) =>{
+    //     // const reason = event.target.value
+    //     // setSelectValue(reason)
+    //     // setForm({
+    //     //     selectValue: event.target.value
+    //     // })
+    // }
+
+    // const toggleError = () =>{
+    //     // setError( (prev) => !prev )
+    //     // setError( (prev) => !prev )
+        
+    // }
 
     return(
         <section>
             <h3>Обратная связь</h3>
 
+            {/* <Button onClick={toggleError}>Toggel error</Button> */}
+
             <form action="">
                 <label htmlFor="name">Ваше имя</label>
-                <input style={{border: error ? '1px solid red' : null }} type="text" id="name" className="control" value={inputValue} onChange={changeValueInput}/>
+                <input style={{border: form.error ? '1px solid red' : null }} type="text" id="name" className="control" value={form.inputValue} onChange={changeValueInput}/>
 
                 <label htmlFor="reason">Причина обращения</label>
-                <select name="" id="reason" className="control" value={selectValue} onChange={changeValueSelect}>
+                <select name="" id="reason" className="control" value={form.selectValue} onChange={ (event) => setForm(prev => ({...prev, selectValue: event.target.value}) )}>
                     <option value="Ошибка">Ошибка</option>
                     <option value="Нужна помощь">Нужна помощь</option>
                     <option value="Предложение">Предложение</option>
                 </select>
             </form>
 
-            <pre>
-                Name: {inputValue}
+           { /* <pre> */} 
+                {/* Name: {form.inputValue}
                 <br />
-                Select: {selectValue}
-            </pre>
+                Select: {form.selectValue} */}
+               {/* {JSON.stringify(form,null,2)} */}
+           { /* </pre> */ }
 
-            <Button disabled={error} isActive={!error}>Отправить</Button>
+            <Button disabled={form.error} isActive={!form.error}>Отправить</Button>
 
             
 
