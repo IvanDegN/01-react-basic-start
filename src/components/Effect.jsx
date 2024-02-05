ac370f1cc3a2d016a2604a86b238be1f4eb31be9
IvanDegN <mauzer.bruno@yandex.ru>
2024-02-05 22:18:30 +0300

    create custom hook, create filter by name, done work with queries to the server

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import Button from './Button/Button'
 import Modal from './Modal/Modal'
+import UseInput from '../hooks/useInput'
+
 
 const Effect = () =>{
 
     const [show, setShow] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([])
+    const input = UseInput()
 
+    const fetchUsers = useCallback( async () => {
+            setLoading(true)
+          const response =  await fetch('https://jsonplaceholder.typicode.com/users')
+          const users = await response.json()
+            setUsers(users)
+            setLoading(false)
+    }, [] )
+
+    useEffect(() => {
+        fetchUsers()
+    },[fetchUsers])
    
-
-    const openModal = () =>{
-        setShow(true)
-    }
-
     return(
         <section>
             <h3>Effect</h3>
 
-            <Button onClick={openModal}>Открыть модальное окно</Button>
+            <Button style={{marginBottom: '1rem'}} onClick={() => setShow(true) }>Открыть модальное окно</Button>
 
             <Modal open={show}>
                 <h3>Hello from modal</h3>
@@ -27,7 +38,16 @@ const Effect = () =>{
             
             
 
-            
+            {loading && <p>Loading...</p>}
+
+            {!loading &&
+            <>
+            <input type="text" className='control' {...input}  /> 
+            <ul>
+                {users.filter(user => user.name.toLowerCase().includes(input.value.toLowerCase())).map(user => <li key={user.id}>{user.name}</li>)}
+            </ul> 
+            </>
+            }
 
         </section>
     )
